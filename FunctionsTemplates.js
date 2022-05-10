c232982de248b438abbe8297d6da779f4a030e3c
Maria Cusell <105199729+MARIACUSELL@users.noreply.github.com>
2022-05-10 13:08:33 +0200

    Actualitzant

--- FunctionsTemplates.js
+++ FunctionsTemplates.js
@@ -1,5 +1,19 @@
+// Nivell 1, Exercici 1
+
 function getName(name) {
   console.log(`El meu nom és ${name}`);
 }
-
 getName("Maria");
+
+// Nivell 2, Exercici 1
+
+let inputName = "Maria";
+let inputUserName = "Cusell";
+
+console.log(`El meu nom és ${inputName} i el meu cognom ${inputUserName}`);
+
+// Nivell 2, Exercici 2
+
+console.log(`${getName("Maria")}`);
+
+// Nivell 3, Exercici 1
